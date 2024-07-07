9eb193df9c749f8c9e5cde0b73f14bedd51f2eb1
mthammus <104604046+mthammus@users.noreply.github.com>
2024-07-07 12:19:31 -0500

    updated scripts

--- stop_services.sh
+++ stop_services.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Navigate to the airbyte/docker directory and start the Docker Compose service
-cd airbyte
+cd /home/ec2-user/modern_data_stack/airbyte
 docker compose stop
 
 # Navigate to the parent directory and then to docker/docker directory, then start the Docker Compose service
-cd ..
+cd /home/ec2-user/modern_data_stack/
 docker compose stop
 docker stop superset
 
