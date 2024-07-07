e1f4fd953b7d2d6e3391b0b27bf1bd00793f646a
mthammus <104604046+mthammus@users.noreply.github.com>
2024-07-06 23:16:47 -0500

    updated scripts

--- setup.sh
+++ setup.sh
@@ -1,115 +1,129 @@
 #!/bin/bash
+# Step to re-execute the script if needed
+if [ "$1" != "second_run" ]; then
+    echo "Initial setup started"
 
-# Function to wait for a container to be healthy
-wait_for_container() {
-    container_name=$1
-    while [ "$(sudo docker inspect -f '{{.State.Health.Status}}' $container_name)" != "healthy" ]; do
-        echo "Waiting for $container_name to be healthy..."
-        sleep 5
-    done
-}
+    echo "Updating all packages..."
+    sudo yum update -y
+    echo "Packages updated."
 
-# Update all packages
-sudo yum update -y
+    echo "Installing git..."
+    sudo yum install git -y
+    echo "Git installed."
 
-# Install git
-sudo yum install git -y
+    echo "Installing Docker..."
+    sudo yum install docker -y
+    echo "Docker installed."
 
-# Install Docker
-sudo yum install docker -y
+    echo "Starting Docker service..."
+    sudo systemctl start docker
+    echo "Docker service started."
 
-# Start Docker service
-sudo systemctl start docker
+    echo "Enabling Docker to start on boot..."
+    sudo systemctl enable docker
+    echo "Docker enabled to start on boot."
 
-# Enable Docker to start on boot
-sudo systemctl enable docker
+    echo "Adding ec2-user to the Docker group..."
+    sudo usermod -aG docker ec2-user
+    echo "ec2-user added to Docker group."
 
-# Add ec2-user to the Docker group
-sudo usermod -aG docker ec2-user
+    echo "Installing Python3 and pip..."
+    sudo yum install python3-pip -y
+    echo "Python3 and pip installed."
 
-# Install Python3 and pip
-sudo yum install python3-pip -y
+    echo "Creating directory for Docker CLI plugins..."
+    sudo mkdir -p /usr/local/lib/docker/cli-plugins
+    echo "Directory for Docker CLI plugins created."
 
-# Create directory for Docker CLI plugins
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
+    echo "Downloading Docker Compose..."
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+    echo "Docker Compose downloaded."
 
-# Download Docker Compose
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+    echo "Making Docker Compose executable..."
+    sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+    echo "Docker Compose is now executable."
 
-# Make Docker Compose executable
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+    echo "Setting appropriate permissions for the Docker directory..."
+    sudo chmod -R 755 /usr/local/lib/docker
+    echo "Permissions set for Docker directory."
 
-# Set appropriate permissions for the Docker directory
-sudo chmod -R 755 /usr/local/lib/docker
+    echo "Installing Nginx..."
+    sudo yum install nginx -y
+    echo "Nginx installed."
 
-# Install Nginx
-sudo yum install nginx -y
+    echo "Creating a welcome page for Nginx..."
+    echo "<h1>Welcome to Secret Data Stack for Amazing Data Projects</h1>" | sudo tee /usr/share/nginx/html/index.html
+    echo "Welcome page created."
 
-# Create a welcome page for Nginx
-echo "<h1>Welcome to Secret Data Stack for Amazing Data Projects</h1>" | sudo tee /usr/share/nginx/html/index.html
+    echo "Starting Nginx service..."
+    sudo systemctl start nginx
+    echo "Nginx service started."
 
-# Start Nginx service
-sudo systemctl start nginx
+    echo "Enabling Nginx to start on boot..."
+    sudo systemctl enable nginx
+    echo "Nginx enabled to start on boot."
 
-# Enable Nginx to start on boot
-sudo systemctl enable nginx
+    echo "Re-executing script with updated group permissions..."
+    exec sg docker "$0 second_run"
+    exit
+fi
 
-# Create a Docker volume for Portainer
-sudo docker volume create portainer_data
+echo "Docker-related operations started"
 
-# Run Portainer container
-sudo docker run -d -p 6200:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce --ssl
+echo "Creating Docker volume for Portainer..."
+docker volume create portainer_data
+echo "Docker volume for Portainer created."
 
-# Wait for Portainer to be healthy
-wait_for_container portainer
+echo "Running Portainer container..."
+docker run -d -p 6200:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce --ssl
+echo "Portainer container started."
 
-# clone modern data stack
+echo "Cloning modern data stack repository..."
 git clone https://github.com/mthammus/modern_data_stack
+echo "Modern data stack repository cloned."
 
-# Navigate into the data stack directory
+echo "Navigating into the data stack directory..."
 cd modern_data_stack
 
-# Clone Airbyte from GitHub
+echo "Cloning Airbyte from GitHub..."
 git clone --depth=1 https://github.com/airbytehq/airbyte.git
+echo "Airbyte cloned."
 
-# Switch into Airbyte directory
+echo "Switching into Airbyte directory..."
 cd airbyte
 
-# Start Airbyte in the background and log output
+echo "Starting Airbyte in the background and logging output..."
 nohup ./run-ab-platform.sh > airbyte.log 2>&1 &
+echo "Airbyte started."
 
-# Wait for Airbyte to be healthy (you may need to adjust this based on how Airbyte reports health)
-sleep 60
-
-# Navigate back to the data stack directory
+echo "Navigating back to the data stack directory..."
 cd ..
 
-# Bring up data stack services with Docker Compose
+echo "Bringing up data stack services with Docker Compose..."
 docker compose up -d postgres mongodb minio minio-setup dremio spark dagster dagster-daemon
+echo "Data stack services started."
 
-# Wait for data stack services to be healthy
-wait_for_container postgres
-wait_for_container mongodb
-wait_for_container minio
-wait_for_container dremio
-wait_for_container spark
-wait_for_container dagster
-wait_for_container dagster-daemon
-
-# Set up Superset
+echo "Setting up Superset..."
 docker run -d -p 8080:8088 -e "SUPERSET_SECRET_KEY=cHnqm6yfvmwZtJLr4rhkkZQoDKP+Rr+ScSPkXWJBbFYmBVpEeod+njXoRjZbu+801tg=" --name superset apache/superset
+echo "Superset container started."
 
-# Wait for Superset to be healthy
-wait_for_container superset
+echo "Waiting for superset"
+sleep 120
 
-# Create Superset admin user
+echo "Creating Superset admin user..."
 docker exec -it superset superset fab create-admin --username mthammu --firstname Manoj --lastname Thammu --email mthammu@terces.io --password SecretDataStack5623#
+echo "Superset admin user created."
 
-# Upgrade Superset database
+echo "Upgrading Superset database..."
 docker exec -it superset superset db upgrade
+echo "Superset database upgraded."
 
-# Load example data into Superset
+echo "Loading example data into Superset..."
 docker exec -it superset superset load_examples
+echo "Example data loaded into Superset."
 
-# Initialize Superset
+echo "Initializing Superset..."
 docker exec -it superset superset init
+echo "Superset initialized."
+
+echo "Setup completed"
